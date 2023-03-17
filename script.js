e6965f0d967eae933ff62a223575bdecc19e916b
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-03-17 19:10:01 +0530

    Complete Updated

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const addTaskBtn = document.getElementById("addtask");
+const btnText = addTaskBtn.innerText;
 const taskField = document.getElementById("task");
 const recordsDisplay = document.getElementById("records")
 
 
+let edit_id = null;
 let taskArray = [];
 
 let objStr = localStorage.getItem('users');
@@ -14,16 +16,25 @@ DisplayInfo();
 
 addTaskBtn.onclick=()=>{
     const text = taskField.value;
-    taskArray.push({'task':text})
+    if(edit_id!=null){
+
+        taskArray.splice(edit_id,1,{'task':text});
+        edit_id=null;
+    }else{
+        
+        taskArray.push({'task':text})
+    }
     SaveInfo(taskArray);
     taskField.value=" ";
-    DisplayInfo();
+    
+    addTaskBtn.innerText = btnText;
 }
 
  
 function SaveInfo(taskArray){
     let str = JSON.stringify(taskArray);
     localStorage.setItem('users', str);
+    DisplayInfo();
 }
 
 function DisplayInfo(){
@@ -32,8 +43,8 @@ function DisplayInfo(){
         statement += `<tr>
         <th scope="row">${i+1}</th>
         <td>${user.task}</td>
-        <td><i class="btn fa fa-edit btn-success text-white mx-2 onclick="EditInfo(${i})"></i> 
-        <i class="fa fa-trash btn text-white btn-danger onclick="DeleteInfo(${i}) "></i></td>
+        <td><i class="btn fa fa-edit btn-success text-white mx-2 " onclick='EditInfo(${i})'></i> 
+        <i class="fa fa-trash btn text-white btn-danger" onclick='DeleteInfo(${i})'></i></td>
       </tr>`
     });
     recordsDisplay.innerHTML=statement; 
@@ -42,10 +53,18 @@ function DisplayInfo(){
 
 }
 
-function EditInfo(){
+function EditInfo(id){
+    // alert(id);
+    edit_id = id;
+    taskField.value = taskArray[id].task;
+    addTaskBtn.innerText="Save Changes"
+
 
 }
 
-function DeleteInfo(){
-
+function DeleteInfo(id){
+    // alert(id);
+    taskArray.splice(id,1);
+    SaveInfo(taskArray);
+    
 }
